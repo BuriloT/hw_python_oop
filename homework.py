648f54cb9737d9dd7f45dd848f2248d5e06ca1a2
Илья <borecki-18@yandex.ru>
2021-10-03 15:21:47 +0300

    Fix after review

--- homework.py
+++ homework.py
@@ -1,69 +1,85 @@
+"""Калькулятор денег и калорий."""
 import datetime as dt
+from typing import Optional, Union
 
 format = '%d.%m.%Y'
 
 
 class Calculator:
-    def __init__(self, limit):
+    """Родительский класс
+    хранит общие функции."""
+    week_date = dt.datetime.today().date() - dt.timedelta(days=7)
+    today_date = dt.date.today()
+
+    def __init__(self, limit: Union[int, float]) -> None:
+        """Конструктор класса Calculator."""
         self.limit = limit
         self.records = []
 
-    def add_record(self, record):
+    def add_record(self, record) -> None:
+        """Сохраняет новую запись."""
         self.records.append(record)
 
-    def get_today_stats(self):
-        count = 0
-        for i in self.records:
-            if i.date == dt.datetime.today().date():
-                count += i.amount
-        return count
+    def get_today_stats(self) -> Union[int, float]:
+        """Считает потраченное за сегодня."""
+        count: int = 0
+        return sum([count + i.amount for i in self.records
+                    if i.date == self.today_date])
 
-    def get_week_stats(self):
-        count = 0
-        today_date = dt.datetime.today().date()
-        week_date = dt.datetime.today().date() - dt.timedelta(days=7)
-        for i in self.records:
-            if today_date >= i.date > week_date:
-                count += i.amount
-        return count
+    def get_week_stats(self) -> Union[int, float]:
+        """Считает потраченное за неделю."""
+        count: int = 0
+        return sum([count + i.amount for i in self.records
+                    if self.today_date >= i.date > self.week_date])
+
+    def get_remained(self) -> Union[int, float]:
+        """Возвращает остаток на сегодня."""
+        self.remained = self.limit - self.get_today_stats()
+        return self.remained
 
 
 class CashCalculator(Calculator):
-    RUB_RATE = 1.0
-    USD_RATE = 60.0
-    EURO_RATE = 70.0
+    """Класс для подсчёта денег."""
+    RUB_RATE: float = 1.0
+    USD_RATE: float = 60.0
+    EURO_RATE: float = 70.0
     currency_dict = {'rub': ('руб', RUB_RATE),
                      'usd': ('USD', USD_RATE),
                      'eur': ('Euro', EURO_RATE)}
 
-    def get_today_cash_remained(self, currency):
+    def get_today_cash_remained(self, currency: str) -> str:
+        """Определяет, сколько ещё денег можно потратить сегодня в
+        рублях, долларах или евро."""
         value, rate = self.currency_dict[currency]
-        cash = self.limit - self.get_today_stats()
-        money = abs(cash / rate)
-        if cash > 0:
-            return f'На сегодня осталось {money:.2f} {value}'
-        elif cash == 0:
+        money: Union[int, float] = abs(self.get_remained() / rate)
+        if self.get_remained() == 0:
             return 'Денег нет, держись'
+        elif self.get_remained() > 0:
+            return f'На сегодня осталось {money:.2f} {value}'
         else:
             return f'Денег нет, держись: твой долг - {money:.2f} {value}'
 
 
 class CaloriesCalculator(Calculator):
-    def get_calories_remained(self):
-        calories = self.limit - self.get_today_stats()
-        if 0 < calories:
+    """Класс для подсчёта калорий."""
+    def get_calories_remained(self) -> str:
+        """Определяет, сколько ещё калорий можно/нужно получить сегодня."""
+        if self.get_remained() > 0:
             return (f'Сегодня можно съесть что-нибудь ещё,'
                     f' но с общей калорийностью не более'
-                    f' {calories} кКал')
+                    f' {self.get_remained()} кКал')
         else:
             return 'Хватит есть!'
 
 
 class Record:
-    def __init__(self, amount, comment, date=None):
+    """Класс для удобства создания записей."""
+    def __init__(self, amount: Union[int, float], comment: str,
+                 date: Optional[str] = None) -> None:
+        """Конструктор класса Record."""
         self.amount = amount
         self.comment = comment
         if date is None:
-            self.date = dt.datetime.today().date()
+            self.date = dt.date.today()
         else:
             self.date = dt.datetime.strptime(date, format).date()
